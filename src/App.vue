d99597ebd15ff5e849c4d7981cce16ec7ddbc9bd
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2024-05-06 16:38:52 +0200

    Add: characters array

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <script>
-export default{
+import axios from "axios";
 
-}
+export default {
+  data() {
+    return {
+      cardsArray: []
+    };
+  },
+  created() {
+    axios.get("https://rickandmortyapi.com/api/character", {
+
+    }).then(resp => {
+      console.log(resp);
+      this.cardsArray = resp.data.results;
+    });
+  },
+};
 </script>
 <template>
-<h1>test</h1>
+  <h1>Rick And Morty App</h1>
 </template>
-<style lang="scss">
-</style>
+<style lang="scss"></style>
